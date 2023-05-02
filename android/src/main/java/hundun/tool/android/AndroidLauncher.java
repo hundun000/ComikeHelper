618ac840fb9b53b241f0aa5c3406c3e71bc31de0
hundun <wwj.hddjj@gmail.com>
2023-05-02 11:46:39 +0800

    android work

---
+++ android/src/main/java/hundun/tool/android/AndroidLauncher.java
@@ -0,0 +1,20 @@
+package hundun.tool.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+import hundun.tool.ComikeHelperGame;
+
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new ComikeHelperGame(new AndroidSaveTool()), configuration);
+    }
+}
---
+++ android/src/main/java/hundun/tool/android/AndroidSaveTool.java
@@ -0,0 +1,26 @@
+package hundun.tool.android;
+
+import hundun.gdxgame.gamelib.base.save.ISaveTool;
+import hundun.tool.logic.data.RootSaveData;
+
+public class AndroidSaveTool implements ISaveTool<RootSaveData> {
+    @Override
+    public void lazyInitOnGameCreate() {
+
+    }
+
+    @Override
+    public boolean hasRootSave() {
+        return false;
+    }
+
+    @Override
+    public void writeRootSaveData(RootSaveData saveData) {
+
+    }
+
+    @Override
+    public RootSaveData readRootSaveData() {
+        return new RootSaveData();
+    }
+}
